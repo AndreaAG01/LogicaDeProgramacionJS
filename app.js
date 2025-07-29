a6107f5666f115184b74d78e8757038c85bbc70b
Andrea AG <agandrea242@gmail.com>
2025-07-28 22:18:23 -0500

    Desafios

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 //Crear un mensaje de alerta
 //alert('Hola mundo');
-prompt('Me indicas un número por favor: ');
+
+//Crear un Input
+//prompt('Me indicas un número por favor: ');
+
+/* Definir Variables (const, let, var) --Sirve para almacenar datos-- */
+//El = es de asignación
+//let numeroUsuario = prompt('Me indicas un número por favor: ');
+
+/* Implementar la ayuda de la consola del navegador */
+//Se puede abrir en el navegador con ctrl+shift+i o F12
+//Las variables se deben llamar tal cual se definen
+//console.log(numeroUsuario);
+
+
+/** Funcionalidad **/
+
+//Se agrupan las variables
+let numeroSecreto = 6;
+let numeroUsuario = prompt('Me indicas un número por favor: ');
+
+//Condicionales (Comparación)
+//Se realiza la comparación con 2 iguales de las variables que tenemos
+if(numeroUsuario == numeroSecreto){
+    alert('Acertaste el número');
+}
---
+++ desafio/desafio.js
@@ -0,0 +1,63 @@
+/*
+Desafíos
+
+1 - Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+
+2 - Declara una variable llamada nombre y asígnale el valor "Lua".
+
+3 - Crea una variable llamada edady asígnale el valor 25.
+
+4 - Establece una variable numeroDeVentas y asígnale el valor 50.
+
+5 - Establece una variable saldoDisponible y asígnale el valor 1000.
+
+6 - Muestra una alerta con el texto "¡Error! Completa todos los campos".
+
+7 - Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
+
+8- Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
+
+9 - Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+
+10 - Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".Si necesitas ayuda, las soluciones a las actividades estarán disponibles en la sección
+
+*/
+
+// 1.
+//alert('¡Bienvenida y bienvenido a nuestro sitio web!');
+
+// 2. 
+/* let nombre = "Lua";
+console.log(nombre); */
+
+// 3. 
+/* let edady = 25;
+console.log(edady); */
+
+// 4.
+/* let numeroDeVentas = 50;
+console.log(numeroDeVentas); */
+
+// 5.
+/* let saldoDisponible = 1000;
+console.log(saldoDisponible); */
+
+// 6.
+//alert('¡Error! Completa todos los campos');
+
+// 7.
+/* let mensajeDeError = '¡Error! Completa todos los campos';
+alert(mensajeDeError); */
+
+// 8.
+/* let nombre;
+nombre = prompt('¿Cuál es tu nombre?'); */
+
+// 9.
+/* let edad;
+edad = prompt('¿Cuál es su edad?'); */
+
+// 10.
+/* if(edad>=18){
+    alert('¡Puedes obtener tu licencia de conducir!');
+} */
